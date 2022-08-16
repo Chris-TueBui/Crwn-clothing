fbb456e06c13d511c228461393e2441e51a4160a
Tue Bui <tuebui2806@gmail.com>
2022-08-16 11:20:47 -0400

    navigation bar and react router

---
+++ src/routes/Navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet,Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.css';
+
+const Navigation = () => {
+    return(
+      <Fragment>
+        <div className="navigation">
+          <Link className="logo-container" to='/'>
+            <CrownLogo className="logo"/>
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to='/shop'>
+              Shop
+            </Link>
+          </div>
+        </div>
+        <Outlet/>
+        {/* this outlet is home.component.jsx */}
+      </Fragment>
+    );
+}
+
+export default Navigation;
